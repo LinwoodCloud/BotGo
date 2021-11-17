689d3dae148afd02c3a3ea74eb83a649edcb811f
CodeDoctorDE <codedoctor@linwood.dev>
2021-11-17 18:12:24 +0100

    add env

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 	"log"
 	"os"
@@ -13,6 +15,30 @@ var s *discordgo.Session
 var database *gorm.DB
 
 func init() { flag.Parse() }
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return !errors.Is(err, os.ErrNotExist)
+}
+
+func init() {
+	if exists(".env") {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+	var err error
+	token := os.Getenv("CHEST_BOT_TOKEN")
+	if token == "" {
+		log.Fatal("CHEST_BOT_TOKEN not set")
+	}
+	s, err = discordgo.New("Bot " + token)
+	database = buildDatabase()
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
 
 var (
 	GuildID = ""
@@ -26,38 +52,7 @@ func commands() []*discordgo.ApplicationCommand {
 	return commands
 }
 
-func setup() {
-	setupCore()
-	setupEconomy()
-}
-
-func main() {
-	var err error
-	s, err = discordgo.New("Bot " + os.Getenv("CHEST_BOT_TOKEN"))
-	database = buildDatabase()
-	if err != nil {
-		log.Fatalf("Invalid bot parameters: %v", err)
-	}
-	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		log.Println("Bot is up!")
-	})
-
-	err = s.Open()
-	if err != nil {
-		log.Fatalf("Cannot open the session: %v", err)
-	}
-	setup()
-
-	commands := commands()
-	cmdIDs := make(map[string]string, len(commands))
-	for _, v := range commands {
-		rcmd, err := s.ApplicationCommandCreate(s.State.User.ID, GuildID, v)
-		if err != nil {
-			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
-		}
-		cmdIDs[rcmd.ID] = rcmd.Name
-	}
-
+func init() {
 	ms := []map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		adminCommandHandlers,
 		economyCommandHandlers,
@@ -75,6 +70,31 @@ func main() {
 			h(s, i)
 		}
 	})
+}
+func setup() {
+	setupCore()
+	setupEconomy()
+}
+
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Println("Bot is up!")
+	})
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+	setup()
+
+	commands := commands()
+	cmdIDs := make(map[string]string, len(commands))
+	for _, v := range commands {
+		rcmd, err := s.ApplicationCommandCreate(s.State.User.ID, GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		cmdIDs[rcmd.ID] = rcmd.Name
+	}
 
 	defer s.Close()
 
